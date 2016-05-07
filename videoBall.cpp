15114177f27f2cd6dea39ed02558a482a5b7ac1e
TPeterW <peterwangtao0@hotmail.com>
2016-05-07 13:27:14 -0400

    Still confused

--- videoBall.cpp
+++ videoBall.cpp
@@ -1,4 +1,5 @@
-/* Joey Button
+/* 
+ * Joey Button
  *
  *
  */
@@ -12,17 +13,17 @@ using namespace std;
 const char *win = "video";
 
 
-void drawCircle( Mat img, Point center )
+void drawCircle(Mat img, Point center)
 {
- int thickness = -1;
- int lineType = 8;
+    int thickness = -1;
+    int lineType = 8;
 
- circle( img,
-         center,
-         32.0,
-         Scalar( 0, 0, 255 ),
-         thickness,
-         lineType );
+    circle( img,
+            center,
+            32.0,
+            Scalar( 0, 0, 255 ),
+            thickness,
+            lineType);
 }
 
 int main()
@@ -30,54 +31,54 @@ int main()
     int cam = 0; // default camera
     VideoCapture cap(cam);
     if (!cap.isOpened()) {
-	fprintf(stderr, "cannot open camera %d\n", cam);
-	exit(1);
+        fprintf(stderr, "cannot open camera %d\n", cam);
+        exit(1);
     }
 
-    //namedWindow(win);
+    // namedWindow(win);
     namedWindow(win, 1);
     Mat frame, screen;
     Point pt;
     pt.x = 500;
     pt.y = 0;
-    int momentumY = 10;
+    int momentumY = 10;     // initial momentum hardcoded as 10
     int momentumX = 0;
 
     while (1) {
         cap >> frame;
         cvtColor(frame, screen, CV_LOAD_IMAGE_COLOR);
-
+        
         pt.x += momentumX;
         pt.y += momentumY;
 
-        if(pt.y<990){
-          momentumY += 1;
-        }else {
-          momentumY = -(momentumY/2);
+        if (pt.y < 990) {                   // not at bottom
+            momentumY += 1;
+        } else {
+            momentumY = -(momentumY * .5);   // bounce back up and halt it
         }
-        if (momentumY*momentumY <= 49 && pt.y > 990){
-          momentumY =0;
+        if (momentumY * momentumY <= 49 && pt.y > 990){
+            momentumY = 0;
         }
 
 
-        //This will zero out the entire image, and draw a red circle
+        // This will zero out the entire image, and draw a red circle
 
         Mat BGRChannels[3];
-        split(screen,BGRChannels);                                  // split the BGR channesl
-        BGRChannels[1]=Mat::zeros(screen.rows,screen.cols,CV_8UC1); // removing Green channel
-        BGRChannels[0]=Mat::zeros(screen.rows,screen.cols,CV_8UC1); // removing Green channel
-        BGRChannels[2]=Mat::zeros(screen.rows,screen.cols,CV_8UC1); // removing Green channel
-        merge(BGRChannels,3,screen);
+        split(screen, BGRChannels);                                  // split the BGR channesl
+        BGRChannels[1] = Mat::zeros(screen.rows, screen.cols, CV_8UC1); // removing Green channel
+        BGRChannels[0] = Mat::zeros(screen.rows, screen.cols, CV_8UC1); // removing Green channel
+        BGRChannels[2] = Mat::zeros(screen.rows, screen.cols, CV_8UC1); // removing Green channel
+        // TODO: what?
+        
+        merge(BGRChannels, 3, screen);
 
-        drawCircle(screen,pt);
+        drawCircle(screen, pt);
+        
 
-
-                            // pack the image
-
-
+        // pack the image
         imshow(win, screen);
         if (waitKey(30) >= 0) // wait up to 30 msec
-	    break;
+	        break;
     }
 
     return 0;
